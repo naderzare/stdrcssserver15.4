2755682329715a5b06ac1923fc067f37da2939c4
naderzare <nader.zare88@gmail.com>
2017-10-05 00:29:52 +0330

    first commit

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+sudo apt-get install g++ build-essential libboost-all-dev qt4-dev-tools libaudio-dev libgtk-3-dev libxt-dev
+
+sudo chmod 777 * -R
+
+./configure
+
+make
+
+sudo make install
+
+sudo mkdir ~/.rcssserver | a
+echo "make .rcssserver"
+mkdir ~/Documents/log | a
+echo "make log"
+
+
+sudo cp server.conf ~/.rcssserver/
+
+sudo apt-add-repository ppa:gnurubuntu/rubuntu
+
+sudo apt-get update
+
+sudo apt-get install rcssmonitor rcsslogplayer
+

0f3eed5f8a5923f03dac164b5a416abcf571813f
Nader Zare <nader.zare88@gmail.com>
2017-10-27 01:18:27 +0330

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+echo "RCSS Server for Student League"
+echo "Technical Committee, Soccer 2D Simulation, IranOpen"
+echo "Nader Zare, nader.zare88@gmail.com"
+
 sudo apt-get install g++ build-essential libboost-all-dev qt4-dev-tools libaudio-dev libgtk-3-dev libxt-dev bison flex
 
 sudo apt-add-repository ppa:gnurubuntu/rubuntu
